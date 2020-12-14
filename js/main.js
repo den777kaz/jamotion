77c6fc9ca82acb3115726b90f6ff8b5c655ec0e4
den777kaz <52310435+den777kaz@users.noreply.github.com>
2020-12-14 20:46:40 +0100

    jaamotion

---
+++ js/main.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // CALL ACTION MODAL WINDOW
+  const closeBtn = document.querySelector('.call-action__closeBtn');
+  const callForm = document.querySelector('.call-action__form');
+  const showForm = document.getElementById('do_show');
+  const formBg = document.querySelector('.call-action__bg');
+  const callModal = document.querySelector('.call-action');
+
+  //open
+  showForm.addEventListener('click', (e) => {
+    openCallModall(e);
+  });
+  //close
+  closeBtn.addEventListener('click', (e) => {
+    closeCallModall(e);
+  });
+
+  function openCallModall(e) {
+    e.preventDefault();
+    showForm.style.display = 'none';
+    closeBtn.style.display = 'flex';
+    formBg.classList.add('move');
+    callForm.classList.add('show');
+    callModal.classList.add('show');
+  }
+  function closeCallModall(e) {
+    e.preventDefault();
+    formBg.classList.remove('move');
+    callForm.classList.remove('show');
+    showForm.style.display = 'flex';
+    closeBtn.style.display = 'none';
+    setTimeout(() => {
+      callModal.classList.remove('show');
+    }, 400);
+  }
+
+  // MUSCLEARM SVG ANIMATION
+});
